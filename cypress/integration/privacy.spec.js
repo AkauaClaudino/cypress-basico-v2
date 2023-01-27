b788d973d1cf3f3c0fe46574e5874c8ec9b41ead
AkauaClaudino <akauaclaudino@outlook.com>
2023-01-27 09:41:17 -0300

    Create cypress project

---
+++ cypress/integration/privacy.spec.js
@@ -0,0 +1,7 @@
+it('Testa a página da politica de privacidade de forma independente', function()  {
+    cy.visit('./src/privacy.html')
+
+    cy.contains('Talking About Testing').should('be.visible')
+
+
+})
